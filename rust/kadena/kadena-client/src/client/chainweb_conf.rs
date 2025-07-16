8fad88def71c6a1a12311747b04ae86d860da9eb
Bogdan Shumygora <bogdan.shumygora@ideasoft.io>
2025-07-16 17:27:15 +0300

    fix: missing slash in hostapi url

--- rust/kadena/kadena-client/src/client/chainweb_conf.rs
+++ rust/kadena/kadena-client/src/client/chainweb_conf.rs
@@ -23,9 +23,13 @@ impl ChainwebConf {
 
     /// Returns the hostapi url for this configuration.
     pub(crate) fn hostapi(&self) -> String {
+        let base = self.url.to_string();
+
         format!(
-            "{}chainweb/0.0/{}/chain/{}/pact",
-            self.url, self.network_id, self.chain_id
+            "{}/chainweb/0.0/{}/chain/{}/pact",
+            base.trim_end_matches('/'),
+            self.network_id,
+            self.chain_id
         )
     }
 }
@@ -38,5 +42,29 @@ impl Default for ChainwebConf {
             network_id: "development".to_string(),
             chain_id: 0,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hostapi() {
+        let conf = ChainwebConf::default();
+        assert_eq!(
+            conf.hostapi(),
+            "https://127.0.0.1:8080/chainweb/0.0/development/chain/0/pact"
+        );
+
+        let conf = ChainwebConf::new(
+            Url::parse("https://api.tatum.io/v3/blockchain/node/kadena-mainnet/t-6749e3a82f9eb24437e59245-5eb4be6f9845425cb1a88875").unwrap(),
+            "mainnet01".to_string(),
+            2,
+        );
+        assert_eq!(
+            conf.hostapi(),
+            "https://api.tatum.io/v3/blockchain/node/kadena-mainnet/t-6749e3a82f9eb24437e59245-5eb4be6f9845425cb1a88875/chainweb/0.0/mainnet01/chain/2/pact"
+        );
     }
 }
